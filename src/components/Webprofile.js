33ff1f90a468247703c38b5c7e6ec2c94ba89e0a
Usama <usamabajwa3030@gmail.com>
2023-10-20 12:13:46 +0500

    webview updated...

--- src/components/Webprofile.js
+++ src/components/Webprofile.js
@@ -1,48 +1,47 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, ActivityIndicator } from 'react-native';
-import { WebView } from 'react-native-webview';
+import React, { useState } from "react";
+import { StyleSheet, View, ActivityIndicator } from "react-native";
+import { WebView } from "react-native-webview";
 
- 
 const WebProfile = () => {
+  const [isLoading, setIsLoading] = useState(true);
 
-    const [isLoading, setIsLoading] = useState(true);
+  const handleLoadEnd = () => {
+    setIsLoading(false);
+  };
 
-    const handleLoadEnd = () => {
-      setIsLoading(false);
-    };
-
-    
   return (
- 
-       <View style={styles.container}>
-       {isLoading && <ActivityIndicator size="large" style={styles.loadingIndicator} />}
-       <WebView
-         source={{ uri: 'https://usama3030.github.io/Portfolio/' }}
-         style={styles.webview}
-         onLoadEnd={handleLoadEnd}
-       />
-     </View>
+    <View style={styles.container}>
+      {isLoading && (
+        <ActivityIndicator size="large" style={styles.loadingIndicator} />
+      )}
+      <WebView
+        //  source={{ uri: 'https://usama3030.github.io/Portfolio/' }}
+        source={{ uri: "https://www.youtube.com/" }}
+        style={styles.webview}
+        onLoadEnd={handleLoadEnd}
+      />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: 'black',
-    },
-    webview: {
-      flex: 1,
-    },
-    loadingIndicator: {
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      right: 0,
-      bottom: 0,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: 'rgba(0, 0, 0, 0.8)',
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "black",
+  },
+  webview: {
+    flex: 1,
+  },
+  loadingIndicator: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.8)",
+  },
+});
 
 export default WebProfile;
--- src/components/webapp.js
+++ src/components/webapp.js
@@ -1,60 +1,61 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, ActivityIndicator } from 'react-native';
-import { WebView } from 'react-native-webview';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, ActivityIndicator } from "react-native";
+import { WebView } from "react-native-webview";
 
-
-
-
-const localhostURL = process.env.REACT_URL;
+//const localhostURL = process.env.REACT_URL;
 
 const WebApp = () => {
+  const [isLoading, setIsLoading] = useState(true);
 
-    const [isLoading, setIsLoading] = useState(true);
+  const handleLoadEnd = () => {
+    setIsLoading(false);
+  };
 
-    const handleLoadEnd = () => {
-      setIsLoading(false);
-    };
-
-    
   return (
- 
-       <View style={styles.container}>
-       {isLoading && <ActivityIndicator size="large" style={styles.loadingIndicator} />}
-       <WebView
-       source={{ uri: localhostURL }}
-         style={styles.webview}
-         onLoadEnd={handleLoadEnd}
-       />
-     </View>
+    <View style={styles.container}>
+      {isLoading && (
+        <ActivityIndicator size="large" style={styles.loadingIndicator} />
+      )}
+      <WebView
+        source={{ uri: "http://192.168.18.42:3000/list" }}
+        style={styles.webview}
+        onLoadEnd={handleLoadEnd}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        onLoad={(event) => console.log("WebView loaded")}
+        onError={(error) => console.error("WebView error:", error)}
+        onMessage={(message) => console.log("WebView message:", message)}
+      />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
+    backgroundColor: "black",
   },
   header: {
-    backgroundColor: 'blue',
+    backgroundColor: "blue",
     paddingVertical: 10,
-    alignItems: 'center',
+    alignItems: "center",
   },
   headerText: {
-    color: 'white',
+    color: "white",
     fontSize: 18,
   },
   webview: {
     flex: 1,
   },
   loadingIndicator: {
-    position: 'absolute',
+    position: "absolute",
     top: 0,
     left: 0,
     right: 0,
     bottom: 0,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.8)',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.8)",
   },
 });
 
