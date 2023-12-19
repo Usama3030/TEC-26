0c7f39c7be6a625627ecb6ee4185cef480d0b151
Usama <usamabajwa3030@gmail.com>
2023-12-19 12:07:00 +0500

    Add bavigation button inside Webview....

--- App.js
+++ App.js
@@ -1,23 +1,129 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { View, StyleSheet, StatusBar } from 'react-native';
-import { globalStyles } from './src/styles/globalStyles';
-import HomeScreen from './src/screens/homeScreen';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import { WebView } from "react-native-webview";
+import { ActivityIndicator, BackHandler } from 'react-native';
+import { TouchableOpacity, Image } from 'react-native';
+import {API_URL} from '@env';
+import { FontAwesome } from '@expo/vector-icons'; 
+
+const Loading = () => <ActivityIndicator
+  style={[styles.container, styles.loading]}
+  color="blue"
+  size="large"
+/>
+
+const WebviewScreen = ({ navigation }) => {
+  const webviewRef = useRef(null);
+  const [canGoBack, setCanGoBack] = useState(false);
+  const [canGoForward, setCanGoForward] = useState(false);
+  const [currentURL, setCurrenetURL] = useState('http://192.168.1.20:3000');
+
+  const backAction = () => {
+ if(canGoBack){
+    webviewRef.current.goBack();
+ }else {
+  navigation.goBack();
+ }
+    return true;
+  }
+
+  const forwardAction = () => {
+    if(webviewRef.current) webviewRef.current.goForward();
+  }
+
+  useEffect(() =>{
+    BackHandler.addEventListener("hardwareBackPress", backAction);
+    () => BackHandler.removeEventListener("hardwareBackPress", backAction);
+  }, [ canGoBack ])
+
+  return (
+    <>
+    <WebView
+    ref= {webviewRef}
+     // source={{ uri: `${API_URL}` }}
+      source={{ uri: currentURL }}
+      style={styles.webview}
+      startInLoadingState
+      renderLoading={Loading}
+      onNavigationStateChange={navState =>{
+        setCanGoBack(navState.canGoBack);
+        setCanGoForward(navState.canGoForward);
+        setCurrenetURL(navState.url)
+      }}
+      javaScriptEnabled={true}
+      domStorageEnabled={true}
+      onLoad={(event) => console.log("WebView loaded:", event.nativeEvent)}
+      onError={(error) => console.error("WebView error:", error)}
+      // onMessage={(message) => console.log("WebView message:", message)}
+//       onMessage={(event) => console.log("WebView message:", event.nativeEvent.data)}
+//       injectedJavaScript={`
+//   document.getElementById('yourSubmitButton').click();
+// `}
+/>
+    <View style= {styles.navigationContainer}>
+<TouchableOpacity disabled={!canGoBack} onPress={backAction} style={styles.navigationButton}>
+  {/* <Text style= {[styles.btn, !canGoBack && {color: 'gray'} ]}>Back</Text> */}
+  <FontAwesome name="chevron-left" size={24} color={canGoBack ? 'black' : 'grey'} />
+</TouchableOpacity>
+<TouchableOpacity disabled={!canGoForward} onPress={forwardAction} style={styles.navigationButton}>
+  {/* <Text style= {[styles.btn, !canGoForward && {color: 'gray'} ]}>Forward</Text> */}
+  <FontAwesome name="chevron-right" size={24} color={canGoForward ? 'black' : 'grey'} />
+</TouchableOpacity>
+    </View>
+    </>
+  )
+}
 
 const Stack = createStackNavigator();
 
 const App = () => {
   console.log("App Executed");
   return (
-
-<NavigationContainer>
-<Stack.Navigator initialRouteName="Home">
-  <Stack.Screen name="Fire Guard" component={HomeScreen} />
-</Stack.Navigator>
-</NavigationContainer>
-
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Fire Guard" component={WebviewScreen}   options={{
+            title: 'Fire Guard',
+            headerStyle: {
+              // backgroundColor: '#3498db',
+              // backgroundColor: '#34495e', 
+              backgroundColor: '#EDF1F2',
+            },
+            headerTintColor: 'black',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
+
+const styles = StyleSheet.create(
+  {
+    container: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    loading: {
+      position: "absolute",
+      height: "100%",
+      width: "100%"
+    },
+    navigationContainer: {
+      // backgroundColor: "#3498db",
+      backgroundColor: "#EDF1F2",
+      padding: 10,
+      justifyContent: "space-around",
+      flexDirection: "row"
+    },
+    navigationButton: {
+      padding: 10,
+      borderRadius: 5,
+    },
+  }
+)
 
 export default App;
--- src/screens/homeScreen.js
+++ src/screens/homeScreen.js
@@ -1,71 +1,103 @@
 
-import React, { useState, useEffect } from "react";
-import { StyleSheet, View, ActivityIndicator } from "react-native";
-import { WebView } from "react-native-webview";
-import {API_URL} from '@env';
-// import {YT_URL} from '@env';
+// import React, { useState, useEffect } from "react";
+// import { StyleSheet, View, ActivityIndicator } from "react-native";
+// import { WebView } from "react-native-webview";
+// import {API_URL} from '@env';
+// // import {YT_URL} from '@env';
+//  import {useNavigation} from '@react-navigation/native';
+
+// const WebApp = () => {
+//   const [isLoading, setIsLoading] = useState(true);
+
+//   const handleLoadEnd = () => {
+//     setIsLoading(false);
+//   };
+//   // console.log(API_URL)
+
+//   const navigation = useNavigation();
+
+//   const goBack = (webview) => {
+//     webview.navigation.goBack();
+//   };
+
+//   return (
+//     <View style={styles.container}>
+//       {isLoading && (
+//         <ActivityIndicator size="large" style={styles.loadingIndicator} />
+//       )}
+//       <WebView
+//        // source={{ uri: `${API_URL}` }}
+//         // source={{ uri: `${YT_URL}` }}
+//         source={{ uri: "http://192.168.10.4:3000" }}
+//         style={styles.webview}
+//         onMessage={(event) => console.log("WebView message:", event.nativeEvent.data)}
+//         injectedJavaScript={`
+//     document.getElementById('yourSubmitButton').click();
+//   `}
+//         onLoadEnd={handleLoadEnd}
+//         javaScriptEnabled={true}
+//         domStorageEnabled={true}
+//         onLoad={(event) => console.log("WebView loaded:", event.nativeEvent)}
+//         onError={(error) => console.error("WebView error:", error)}
+//         // onMessage={(message) => console.log("WebView message:", message)}
+//       />
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "black",
+//   },
+//   header: {
+//     backgroundColor: "blue",
+//     paddingVertical: 10,
+//     alignItems: "center",
+//   },
+//   headerText: {
+//     color: "white",
+//     fontSize: 18,
+//   },
+//   webview: {
+//     flex: 1,
+//   },
+//   loadingIndicator: {
+//     position: "absolute",
+//     top: 0,
+//     left: 0,
+//     right: 0,
+//     bottom: 0,
+//     justifyContent: "center",
+//     alignItems: "center",
+//     backgroundColor: "rgba(0, 0, 0, 0.8)",
+//   },
+// });
+
+// export default WebApp;
 
 
-const WebApp = () => {
-  const [isLoading, setIsLoading] = useState(true);
 
-  const handleLoadEnd = () => {
-    setIsLoading(false);
-  };
-  // console.log(API_URL)
+// import React from 'react';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+// import { View, StyleSheet, StatusBar } from 'react-native';
+// import { globalStyles } from './src/styles/globalStyles';
+// import HomeScreen from './src/screens/homeScreen';
 
-  return (
-    <View style={styles.container}>
-      {isLoading && (
-        <ActivityIndicator size="large" style={styles.loadingIndicator} />
-      )}
-      <WebView
-        source={{ uri: `${API_URL}` }}
-        // source={{ uri: `${YT_URL}` }}
-       // source={{ uri: "http://192.168.1.20:3000" }}
-        style={styles.webview}
-        onMessage={(event) => console.log("WebView message:", event.nativeEvent.data)}
-        injectedJavaScript={`
-    document.getElementById('yourSubmitButton').click();
-  `}
-        onLoadEnd={handleLoadEnd}
-        javaScriptEnabled={true}
-        domStorageEnabled={true}
-        onLoad={(event) => console.log("WebView loaded:", event.nativeEvent)}
-        onError={(error) => console.error("WebView error:", error)}
-        // onMessage={(message) => console.log("WebView message:", message)}
-      />
-    </View>
-  );
-};
+// const Stack = createStackNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "black",
-  },
-  header: {
-    backgroundColor: "blue",
-    paddingVertical: 10,
-    alignItems: "center",
-  },
-  headerText: {
-    color: "white",
-    fontSize: 18,
-  },
-  webview: {
-    flex: 1,
-  },
-  loadingIndicator: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "rgba(0, 0, 0, 0.8)",
-  },
-});
+// const App = () => {
+//   console.log("App Executed");
+//   return (
 
-export default WebApp;
+// <NavigationContainer>
+// <Stack.Navigator initialRouteName="Home">
+//   <Stack.Screen name="Fire Guard" component={HomeScreen} />
+// </Stack.Navigator>
+// </NavigationContainer>
+
+//   );
+// };
+
+// export default App;
